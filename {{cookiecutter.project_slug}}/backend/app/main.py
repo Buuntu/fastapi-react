0523d19cad6e1d796378c1868060e0ff36a6fd30
Gabe Abud <gabriel.jabud@gmail.com>
2020-05-13 11:11:57 -0700

    add api route as root url

--- {{cookiecutter.project_slug}}/backend/app/main.py
+++ {{cookiecutter.project_slug}}/backend/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from starlette.requests import Request
+import uvicorn
 
 from app.core import config
 from app.db.session import SessionLocal
 
 app = FastAPI(
     title=config.PROJECT_NAME,
-    openapi_url="/api/v1/openapi.json",
+    docs_url="/api/docs", openapi_url="/api"
 )
 
 
@@ -21,3 +22,9 @@ async def db_session_middleware(request: Request, call_next):
 @app.get("/api/v1")
 async def root():
     return {"message": "Hello World"}
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app", host="0.0.0.0", reload=True, port=8888, root_path="/api"
+    )
