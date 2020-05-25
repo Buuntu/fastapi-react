f1f39b5a997e0736117d029332bc87510ae17ab0
Gabe Abud <gabriel.jabud@gmail.com>
2020-05-25 08:30:08 -0700

    fix weird styling in admin page

--- {{cookiecutter.project_slug}}/frontend/src/App.tsx
+++ {{cookiecutter.project_slug}}/frontend/src/App.tsx
@@ -1,33 +1,6 @@
 import React, { FC } from 'react';
 import { Routes } from './Routes';
-import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
-  app: {
-    textAlign: 'center',
-  },
-  header: {
-    backgroundColor: '#282c34',
-    minHeight: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 'calc(10px + 2vmin)',
-    color: 'white',
-  },
-}));
-
-const App: FC = () => {
-  const classes = useStyles();
-
-  return (
-    <div className={classes.app}>
-      <header className={classes.header}>
-        <Routes />
-      </header>
-    </div>
-  );
-};
+const App: FC = () => <Routes />;
 
 export default App;
--- {{cookiecutter.project_slug}}/frontend/src/Routes.tsx
+++ {{cookiecutter.project_slug}}/frontend/src/Routes.tsx
@@ -1,21 +1,45 @@
 import React, { FC } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { Home, Login } from './views';
 import { Admin } from './admin';
 
+const useStyles = makeStyles((theme) => ({
+  app: {
+    textAlign: 'center',
+  },
+  header: {
+    backgroundColor: '#282c34',
+    minHeight: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    fontSize: 'calc(10px + 2vmin)',
+    color: 'white',
+  },
+}));
+
 export const Routes = () => {
+  const classes = useStyles();
+
   return (
     <Switch>
       <Route path="/admin">
         <Admin />
       </Route>
-      <Route path="/login">
-        <Login />
-      </Route>
-      <Route path="/">
-        <Home />
-      </Route>
+
+      <div className={classes.app}>
+        <header className={classes.header}>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </header>
+      </div>
     </Switch>
   );
 };
