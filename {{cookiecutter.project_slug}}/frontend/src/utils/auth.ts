deb51ce3eb6b5fe1e219cc2e6aa3049ae60b3299
Gabe Abud <gabriel.jabud@gmail.com>
2020-05-25 11:04:19 -0700

    add isAuthenticated function that checks local storage permissions

--- {{cookiecutter.project_slug}}/frontend/src/utils/auth.ts
+++ {{cookiecutter.project_slug}}/frontend/src/utils/auth.ts
@@ -1,7 +1,14 @@
 export const isAuthenticated = () => {
-  return true;
+  const permissions = localStorage.getItem('permissions');
+  if (!permissions) {
+    return false;
+  }
+  return permissions === 'user' || permissions === 'admin' ? true : false;
 };
 
 export const login = (email: string, password: string) => {};
 
-export const logout = () => {};
+export const logout = () => {
+  localStorage.removeItem('token');
+  localStorage.removeItem('permissions');
+};
