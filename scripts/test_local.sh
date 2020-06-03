62e7420bee33fa2b6cde3e6f94512dc3b5bb9548
freddiehonohan <honohanf@gmail.com>
2020-06-03 22:38:32 +0100

    Update test_local.sh

--- scripts/test_local.sh
+++ scripts/test_local.sh
@@ -5,7 +5,7 @@ cd ..
 current_dir=`pwd`
 cd ..
 ./fastapi-react/scripts/dev-project.sh
-cd fastapi-react-dev
+cd dev-fastapi-react
 docker-compose down -v --remove-orphans
 ./scripts/build.sh
 ./scripts/test.sh
