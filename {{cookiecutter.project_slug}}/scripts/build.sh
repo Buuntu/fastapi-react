4199e75b899d6b2e6d535bfa20e4691bf3524e9c
Maxim Napad <m.napad@potok.io>
2021-06-12 15:45:21 +0300

    add stop scripts on error

--- {{cookiecutter.project_slug}}/scripts/build.sh
+++ {{cookiecutter.project_slug}}/scripts/build.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Exit in case of error
+set -e
+
 # Build and run containers
 docker-compose up -d
 
--- {{cookiecutter.project_slug}}/scripts/test_backend.sh
+++ {{cookiecutter.project_slug}}/scripts/test_backend.sh
@@ -1,3 +1,6 @@
 #! /usr/bin/env bash
 
+# Exit in case of error
+set -e
+
 docker-compose run backend pytest $@
