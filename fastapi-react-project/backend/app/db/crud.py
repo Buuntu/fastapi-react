36e9eb3e6687857a0e0ea8cb812683f803e57c17
SAM Jubayer <jubayer1971@gmail.com>
2023-06-14 17:06:10 +0900

    Feature: Implement CRUD operations for Notes
    - Add Note model in the backend with properties: id, title, and description.
    - Create Note endpoints in the backend: get, post, put, delete.
    - Create Note components in the frontend: NoteForm for create and update, NoteList for displaying all notes.
    - Handle frontend routing with react-router-dom.

---
+++ fastapi-react-project/backend/app/db/crud.py
@@ -0,0 +1,119 @@
+from fastapi import HTTPException, status
+from sqlalchemy.orm import Session
+import typing as t
+
+from . import models, schemas
+from app.core.security import get_password_hash
+
+
+def get_user(db: Session, user_id: int):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if not user:
+        raise HTTPException(status_code=404, detail="User not found")
+    return user
+
+
+def get_user_by_email(db: Session, email: str) -> schemas.UserBase:
+    return db.query(models.User).filter(models.User.email == email).first()
+
+
+def get_users(
+    db: Session, skip: int = 0, limit: int = 100
+) -> t.List[schemas.UserOut]:
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = get_password_hash(user.password)
+    db_user = models.User(
+        first_name=user.first_name,
+        last_name=user.last_name,
+        email=user.email,
+        is_active=user.is_active,
+        is_superuser=user.is_superuser,
+        hashed_password=hashed_password,
+    )
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+
+def delete_user(db: Session, user_id: int):
+    user = get_user(db, user_id)
+    if not user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+    db.delete(user)
+    db.commit()
+    return user
+
+
+def edit_user(
+    db: Session, user_id: int, user: schemas.UserEdit
+) -> schemas.User:
+    db_user = get_user(db, user_id)
+    if not db_user:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="User not found")
+    update_data = user.dict(exclude_unset=True)
+
+    if "password" in update_data:
+        update_data["hashed_password"] = get_password_hash(user.password)
+        del update_data["password"]
+
+    for key, value in update_data.items():
+        setattr(db_user, key, value)
+
+    db.add(db_user)
+    db.commit()
+    db.refresh(db_user)
+    return db_user
+
+def get_note(db: Session, note_id: int):
+    note = db.query(models.Note).filter(models.Note.id == note_id).first()
+    if not note:
+        raise HTTPException(status_code=404, detail="Note not found")
+    return note
+
+
+def get_notes(
+    db: Session, skip: int = 0, limit: int = 100
+) -> t.List[schemas.NoteOut]:
+    return db.query(models.Note).offset(skip).limit(limit).all()
+
+
+def create_note(db: Session, note: schemas.NoteCreate, user_id: int):
+    db_note = models.Note(
+        title=note.title,
+        description=note.description,
+        user_id=user_id,
+    )
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
+
+
+def delete_note(db: Session, note_id: int):
+    note = get_note(db, note_id)
+    if not note:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Note not found")
+    db.delete(note)
+    db.commit()
+    return note
+
+
+def edit_note(
+    db: Session, note_id: int, note: schemas.NoteEdit
+) -> schemas.Note:
+    db_note = get_note(db, note_id)
+    if not db_note:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, detail="Note not found")
+    update_data = note.dict(exclude_unset=True)
+
+    for key, value in update_data.items():
+        setattr(db_note, key, value)
+
+    db.add(db_note)
+    db.commit()
+    db.refresh(db_note)
+    return db_note
